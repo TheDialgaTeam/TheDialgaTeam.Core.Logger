124826e1d5abd9bc1490120e4e0bb8e77cb47a07
Yong Jian Ming <jianming1993@gmail.com>
2021-05-12 01:48:11 +0800

    Change ActionSink implementation

--- src/Sink/ActionSink.cs
+++ src/Sink/ActionSink.cs
@@ -6,42 +6,27 @@ using Serilog.Formatting;
 
 namespace TheDialgaTeam.Core.Logger.Sink
 {
-    public class ActionSink : ILogEventSink, IDisposable
+    public class ActionSink : ILogEventSink
     {
         private readonly Action<string> _outputAction;
         private readonly ITextFormatter _formatter;
         private readonly object _syncRoot;
-
-        private readonly MemoryStream _memoryStream = new MemoryStream();
-        private readonly StreamWriter _streamWriter;
-        private readonly StreamReader _streamReader;
 
         public ActionSink(Action<string> outputAction, ITextFormatter formatter, object syncRoot)
         {
             _outputAction = outputAction;
             _formatter = formatter;
             _syncRoot = syncRoot;
-            _streamWriter = new StreamWriter(_memoryStream);
-            _streamReader = new StreamReader(_memoryStream);
         }
 
         public void Emit(LogEvent logEvent)
         {
             lock (_syncRoot)
             {
-                _memoryStream.Seek(0, SeekOrigin.Begin);
-                _memoryStream.SetLength(0);
-
-                _formatter.Format(logEvent, _streamWriter);
-
-                _memoryStream.Seek(0, SeekOrigin.Begin);
-                _outputAction(_streamReader.ReadToEnd());
+                using var stringWriter = new StringWriter();
+                _formatter.Format(logEvent, stringWriter);
+                _outputAction(stringWriter.ToString());
             }
-        }
-
-        public void Dispose()
-        {
-            _memoryStream.Dispose();
         }
     }
 }
